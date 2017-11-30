d9d737ad79a65b15dcfbfdc84861ebf1799f1fa3
Jonathan Rodriguez <clubzspadez@gmail.com>
2017-11-29 22:21:22 -0500

    updated tests for Initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,6 +84,7 @@ $(function() {
 
     });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -91,7 +92,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(1, done);
+         });
 
+         it('should be at least 1 .entry element within .feed', function(){
+            var feedNum = $('.feed').find('.entry').length;
+            expect(feedNum).toBeGreaterThan(0);
+         })
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
